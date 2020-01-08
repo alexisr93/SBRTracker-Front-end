f1424208c4f7b32b8f6eaa734cbb55676895ba67
Alexis <alexisr1993@gmail.com>
2020-01-07 20:18:08 -0800

    Minor changes to DashBoard

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -14,17 +14,36 @@ class DashBoard extends React.Component {
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h2 className="h2">Dashboard</h2>
         </div>
+
         <div className="row">
           <div className="col">
-            <h3>Pending: </h3>
+            <div class="card">
+              <div class="card-body">
+                <h3 class="card-title">Pending:</h3>
+                <p class="card-text">Sample Text</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div class="card">
+              <div class="card-body">
+                <h3 class="card-title">Resolved Today:</h3>
+                <p class="card-text">Sample Text</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <h3>Resolved Today: </h3>
-          </div>
-          <div className="col">
-            <h3>Resolved This Week: </h3>
+            <div class="card">
+              <div class="card-body">
+                <h3 class="card-title">Resolved This Week:</h3>
+                <p class="card-text">Sample Text</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
