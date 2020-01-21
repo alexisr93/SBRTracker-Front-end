371581528b6efd33c61da012e2d5caad6d3e760b
Alexis <alexisr1993@gmail.com>
2020-01-20 21:01:00 -0800

    Created file

---
+++ src/SearchResult.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+export default SearchResult;
