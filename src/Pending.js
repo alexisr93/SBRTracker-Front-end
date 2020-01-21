7bdfcfded42b7d910a2f893edd8ab3c75011bffb
Alexis <alexisr1993@gmail.com>
2020-01-20 22:20:48 -0800

    Refactored

--- src/Pending.js
+++ src/Pending.js
@@ -47,33 +47,47 @@ class Pending extends React.Component {
 
   render() {
     const items = [];
-    if(this.state.items)
-    this.state.items.forEach(item => {
-      if(item.status === "Pending"){
-        items.push(
-          <tr key={item.id}>
-            <td>{item.first_name}</td>
-            <td>{item.middle_name}</td>
-            <td>{item.last_name}</td>
-            <td>{item.grade}</td>
-            <td>{item.class_period}</td>
-            <td>{item.date_of_incident}</td>
-            <td>{item.incident_type}</td>
-            <td>{item.previous_intervention}</td>
-            <td>{item.incident_description}</td>
-            <td>
-              <button type="submit" className="btn btn-dark" name={item.id} onClick={this.handleClick}>Delete</button>
-              <Link to={{
-                pathname: "/update",
-                param1: item.id
-              }}>
-              <button type="button" className="btn btn-secondary">Update</button></Link>
-            </td>
-          </tr>
-        )
-      }
 
-    })
+    if (this.state.items) {
+      this.state.items.forEach(item => {
+        if(item.status === "Pending"){
+          items.push(
+            <tr key={item.id}>
+              <td>{item.first_name}</td>
+              <td>{item.middle_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.grade}</td>
+              <td>{item.class_period}</td>
+              <td>{item.date_of_incident}</td>
+              <td>{item.incident_type}</td>
+              <td>{item.previous_intervention}</td>
+              <td>{item.incident_description}</td>
+              <td>
+                <button
+                  type="submit"
+                  className="btn btn-dark"
+                  name={item.id}
+                  onClick={this.handleClick}>
+                  Delete
+                </button>
+                <Link
+                  to={{
+                    pathname: "/update",
+                    param1: item.id
+                  }}>
+                    <button
+                      type="button"
+                      className="btn btn-secondary">
+                      Update
+                    </button>
+                </Link>
+              </td>
+            </tr>
+          )
+        }
+      });
+    }
+
     return (
       <div>
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
